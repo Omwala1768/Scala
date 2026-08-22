cb016daaa5b4abd25de17778af9fa25959c46667
Omwala1768 <walaomcs252658@gmail.com>
2026-08-22 22:48:07 +0530

    Update omwala_M2_pract10.py

--- omwala_M2_pract10.py
+++ omwala_M2_pract10.py
@@ -1,31 +1,24 @@
 from pyspark.sql import SparkSession
 
-# Create Spark session
 spark = SparkSession.builder \
     .appName("FilterRows") \
     .master("local[*]") \
     .getOrCreate()
 
-# Read the CSV file
 df = spark.read.csv(
     "students.csv",
     header=True,
     inferSchema=True
 )
 
-# Display the original data
 print("Original Data:")
 df.show()
 
-# Set the threshold
 threshold = 75
 
-# Filter rows where Marks is greater than the threshold
 filtered_df = df.filter(df["Marks"] > threshold)
 
-# Display the filtered data
 print("Students with Marks greater than", threshold)
 filtered_df.show()
 
-# Stop Spark
 spark.stop()
